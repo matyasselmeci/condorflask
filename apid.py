0104cdcbc0276fc5617813a4bf0fa2f19cd2b1f3
Matyas Selmeci <matyas@cs.wisc.edu>
2019-06-20 13:03:08 -0500

    Add Config

--- apid.py
+++ apid.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import json
 
@@ -61,5 +62,42 @@ class HistoryResource(JobsBaseResource):
     executable = "condor_history"
 
 
+class ConfigResource(Resource):
+    def get(self, attribute=None):
+        parser = reqparse.RequestParser(trim=True)
+        parser.add_argument("daemon", choices=["master", "schedd", "startd", "collector", "negotiator"])
+        args = parser.parse_args()
+
+        cmd = ["condor_config_val", "-raw"]
+        if args.daemon:
+            cmd.append("-%s" % args.daemon)
+
+        if attribute:
+            cmd.append(attribute)
+        else:
+            cmd.append("-dump")
+
+        completed = subprocess.run(cmd, capture_output=True, encoding="utf-8")
+        if completed.returncode != 0:
+            # lazy
+            if re.search(r"^Not defined:", completed.stderr, re.MULTILINE):
+                abort(404, message=completed.stderr)
+            else:
+                abort(400, message=completed.stderr)
+
+        data = {}
+        if attribute:
+            data[attribute.lower()] = completed.stdout.rstrip("\n")
+        else:
+            for line in completed.stdout.split("\n"):
+                if line.startswith("#"): continue
+                if " = " not in line: continue
+                key, value = line.split(" = ", 1)
+                data[key.lower()] = value
+
+        return data
+
+
 api.add_resource(JobsResource, "/v1/jobs", "/v1/jobs/<int:clusterid>", "/v1/jobs/<int:clusterid>/<int:procid>")
 api.add_resource(HistoryResource, "/v1/history", "/v1/history/<int:clusterid>", "/v1/history/<int:clusterid>/<int:procid>")
+api.add_resource(ConfigResource, "/v1/config", "/v1/config/<attribute>")
