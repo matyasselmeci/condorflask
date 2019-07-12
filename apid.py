c665db2bdba45c75d88488d243260d68701de2e8
Matyas Selmeci <matyas@cs.wisc.edu>
2019-07-12 14:54:40 -0500

    Toss in a condor_status endpoint

--- apid.py
+++ apid.py
@@ -74,6 +74,47 @@ class HistoryResource(JobsBaseResource):
     executable = "condor_history"
 
 
+class StatusResource(Resource):
+    def get(self, name=None):
+        parser = reqparse.RequestParser(trim=True)
+        parser.add_argument("projection", default="")
+        parser.add_argument("constraint", default="")
+        args = parser.parse_args()
+
+        cmd = ["condor_status", "-json"]
+
+        if name:
+            cmd.append(name)
+
+        if args.constraint:
+            cmd.extend(["-constraint", args.constraint])
+        if args.projection:
+            cmd.extend(["-attributes", args.projection + ",name"])
+
+        completed = subprocess.run(cmd, capture_output=True, encoding="utf-8")
+        if completed.returncode != 0:
+            # lazy
+            if re.search(r"^condor_status: unknown host", completed.stderr, re.MULTILINE):
+                abort(404, message=completed.stderr)
+            else:
+                abort(400, message=completed.stderr)
+
+        # super lazy here - the real deal would use the API anyway
+        classads = json.loads(completed.stdout)
+        if not classads:
+            abort(404, message="No ad(s) found")
+
+        # lowercase all the keys
+        classads_lower = [{k.lower(): v for k, v in ad.items()} for ad in classads]
+
+        data = [
+            {"name": ad["name"],
+             "classad": ad} for ad in classads_lower
+        ]
+
+        return data
+
+
 class ConfigResource(Resource):
     def get(self, attribute=None):
         parser = reqparse.RequestParser(trim=True)
@@ -116,4 +157,5 @@ api.add_resource(JobsResource, "/v1/jobs", "/v1/jobs/<int:clusterid>",
 api.add_resource(HistoryResource, "/v1/history", "/v1/history/<int:clusterid>",
                  "/v1/history/<int:clusterid>/<int:procid>",
                  "/v1/history/<int:clusterid>/<int:procid>/<attribute>")
+api.add_resource(StatusResource, "/v1/status", "/v1/status/<name>")
 api.add_resource(ConfigResource, "/v1/config", "/v1/config/<attribute>")
